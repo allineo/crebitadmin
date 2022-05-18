2bec687a8afb4fd5fcc0c3dc6384cfb58c7b17f9
Alline Oliveira allineo <alline.oliveira@gmail.com>
2022-05-18 12:22:46 -0300

    'alias'

--- src/liveonaccount.js
+++ src/liveonaccount.js
@@ -11,36 +11,69 @@ let liveonCredentials = {
 };
 
 
-function getTokenRequestOptions(cpf) {
+function getTokenRequestOptions() {
     var header = {
+        //'Subscription-key': subscriptionkey,
         'Content-Type': 'application/json',
-        'Subscription-key': liveonCredentials['subscriptionKey']
     };
-    var access = cpf.substring(3, 9).split("").reverse().join("");
-    var dados = JSON.stringify({
-        "document": cpf,
-        "password": access
+    var data = JSON.stringify({
+        "document": "14090172730",
+        "password": "727109"
     });
     var requestOptions = {
         method: 'POST',
         headers: header,
-        body: dados,
+        body: data,
         redirect: 'follow'
     };
     return requestOptions;
 }
 
-function getToken(cpf) {
-    var requestOptions = getTokenRequestOptions(cpf);
-    fetch(liveonCredentials['urlProxy'] + "/auth", requestOptions)
+function getToken() {
+    var requestOptions = getTokenRequestOptions()
+    fetch("https://proxy.apps.binnovation.co/crebit/auth", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
 }
 
 
-export const getAccountInfo = async function (cpf) {
-    const token = await getToken(cpf);
-    console.log(token);
-    document.getElementById('resposta').innerHTML = JSON.stringify(token);
+exports.getAccountInfo =  function (cpf) {
+   // document.getElementById("messageField").innerHTML = 'Aguarde...';
+   // let code = document.getElementById("codeField").value;
+    //let token = getToken();
+    const urlAuth = "https://proxy.apps.binnovation.co/crebit/auth";
+    var requestOptions = getTokenRequestOptions();
+    fetch(urlAuth, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            let token = result.token;
+            console.log(token);
+            const urlValidateCode = 'https://proxy.apps.binnovation.co/crebit' + '/pix/dict/validate_code';
+            /*var header = {
+                //'Subscription-key': subscriptionkey,
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json',
+            };
+            var data = JSON.stringify({
+                "key": '+' + phone,
+                "key_type": "phone",
+                "code": code
+            });
+            var requestOptions = {
+                method: 'POST',
+                headers: header,
+                body: data,
+                redirect: 'follow'
+            };
+            fetch(urlValidateCode, requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    console.log(result)
+                    exibeMensagem(result);
+                })
+                .catch(error => console.log('error', error));*/
+        })
+        .catch(error => console.log('error', error));
 }
